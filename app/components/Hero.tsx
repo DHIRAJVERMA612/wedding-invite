546f3c5a310f0de85aecfb818eea3ef654fbd79d
Dhiraj Kumar <dhirajverma612@gmail.com>
2026-05-02 17:57:03 +0530

    Added animated PNG in hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,41 +1,3 @@
-// "use client";
-
-// import { motion } from "framer-motion";
-
-// export default function Hero() {
-//   return (
-//     <div className="h-screen flex flex-col justify-center items-center bg-pink-100 text-center">
-      
-//       <motion.h1
-//         className="text-4xl md:text-5xl font-bold text-pink-600"
-//         initial={{ opacity: 0, y: -50 }}
-//         animate={{ opacity: 1, y: 0 }}
-//         transition={{ duration: 1 }}
-//       >
-//         Dhiraj ❤️ Saroj
-//       </motion.h1>
-
-//       <motion.p
-//         className="mt-4 text-xl text-gray-800"
-//         initial={{ opacity: 0 }}
-//         animate={{ opacity: 1 }}
-//         transition={{ delay: 1 }}
-//       >
-//         We are getting married!
-//       </motion.p>
-
-//       <motion.p
-//         className="mt-2 text-lg text-gray-700"
-//         initial={{ opacity: 0 }}
-//         animate={{ opacity: 1 }}
-//         transition={{ delay: 1.5 }}
-//       >
-//         10 December 2026
-//       </motion.p>
-
-//     </div>
-//   );
-// }
 "use client";
 
 import { motion } from "framer-motion";
@@ -74,6 +36,7 @@ export default function Hero() {
         >
           We are getting married!
         </motion.p>
+
         <motion.p
           className="mt-2 text-lg"
           initial={{ opacity: 0 }}
@@ -85,13 +48,14 @@ export default function Hero() {
         <motion.img
           src="/images/ring.png"
           alt="ring"
-          className="mt-6 w-64 h-64 mx-auto"
+          className="mt-6 w-40 h-64 mx-auto"
           animate={{ rotate: [-5, 5, -5] }}
           transition={{
             duration: 0.6,
             repeat: Infinity,
             ease: "easeInOut",
           }}
+        />
 
       </div>
 
