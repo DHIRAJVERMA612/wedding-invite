b0c1e125e299895cba0517e702907d5c4653012c
Dhiraj Kumar <dhirajverma612@gmail.com>
2026-09-26 11:08:24 +0530

    photo update

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 
 export default function Gallery() {
   const images = [
-    "/images/couple__1.jpg",
-    "/images/couple__2.jpg",
-    "/images/couple__3.jpg",
+    "/images/couple1.jpg",
+    "/images/couple2.jpg",
+    "/images/couple3.jpg",
   ];
 
   return (
